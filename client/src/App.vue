f456883e9a80166c55f53464e99d7ba02d020de7
Tuan Anh Tran Van <anh.tran@tomosisa.com>
2023-09-18 22:41:57 +0700

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <game-container></game-container>
+</template>
+
+<script>
+import GameContainer from './components/GameContainer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'game-container': GameContainer
+  }
+}
+</script>
+
+<style>
+#app {
+  margin-top: 0;
+}
+</style>
---
+++ client/src/components/PhaserContainer.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div :id="containerId" />
+</template>
+
+<script setup>
+import { onMounted, onUnmounted } from 'vue'
+
+let gameInstance = null
+const containerId = 'game-container'
+const game = await import('@/game/game')
+
+onMounted(() => {
+  gameInstance = game.launch(containerId)
+})
+
+onUnmounted(() => {
+  gameInstance.destroy(false)
+})
+</script>
+
